b83deb971dabf76a8629997f5d09308dfd1e4f88
xocass <joaquin.quintas@rai.usc.es>
2025-10-08 09:51:39 +0200

    commit 0

---
+++ src/main/java/gal/usc/etse/sharecloud/ShareCloudApplication.java
@@ -0,0 +1,13 @@
+package gal.usc.etse.sharecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gal/usc/etse/sharecloud/ShareCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package gal.usc.etse.sharecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

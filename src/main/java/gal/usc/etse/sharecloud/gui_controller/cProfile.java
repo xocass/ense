4924275e6eef8ad8e1830290dd9c089564129e49
xocass <joaquin.quintas@rai.usc.es>
2025-11-12 11:05:58 +0100

    añadido beans, cambiado module-info

---
+++ src/main/java/gal/usc/etse/sharecloud/gui_controller/cProfile.java
@@ -0,0 +1,34 @@
+package gal.usc.etse.sharecloud.gui_controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class cProfile {
+    @FXML
+    private Text init;
+    @FXML
+    private Text friends;
+    @FXML
+    private Text profile;
+    @FXML
+    private Text logOut;
+
+
+    @FXML
+    public void clickInit(){
+
+    }
+    @FXML
+    public void clickFriends(){
+
+    }
+    @FXML
+    public void clickProfile(){
+
+    }
+    @FXML
+    public void logOut(){
+
+    }
+
+}
